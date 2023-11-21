cfd89f1dfa1cfe01bafa58c57c66869465eff83f
FilipSzkarlat <filip.szkarlat@gmail.com>
2023-11-21 13:37:45 +0100

    Delete Book from List

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 };
 
+// Delete Book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === "delete") {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 // Clear Fields
 UI.prototype.clearFields = function () {
   document.getElementById("title").value = "";
@@ -52,7 +59,7 @@ UI.prototype.clearFields = function () {
   document.getElementById("isbn").value = "";
 };
 
-// Event Listeners
+// Event Listeners for add book
 document.getElementById("book-form").addEventListener("submit", function (e) {
   // Get form values
   const title = document.getElementById("title").value,
@@ -80,5 +87,20 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
     ui.clearFields();
   }
 
+  e.preventDefault();
+});
+
+// Event Listner for delete
+document.getElementById("book-list").addEventListener("click", function (e) {
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert("Book removed!", "success");
+
+  console.log("test");
   e.preventDefault();
 });
